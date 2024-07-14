b11bbb85407f72f457319f834ceb056163ae7323
meherunnesaenta <124086712+meherunnesaenta@users.noreply.github.com>
2024-07-14 20:33:03 +0600

    Create matrix trasnpose.cpp

---
+++ 2D Array/matrix trasnpose.cpp
@@ -0,0 +1,27 @@
+//matrix trasnpse
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>a[i][j];
+
+        }
+    }int b[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+        int temp=a[i][j];
+        a[i][j]=a[j][i];
+        a[j][i]=temp;
+           
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+           cout<<a[i][j]<<" ";             
+        }cout<<endl;
+    }
+}
